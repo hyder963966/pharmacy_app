5d76cac3c514587128ae3e578c93e660baf65602
hyder963966 <hydrlmhmwd@gmail.com>
2026-05-15 23:31:46 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,4 +1,10 @@
 pluginManagement {
+    // الحصول على مسار Flutter SDK الذي يمرره أمر "flutter build" تلقائياً
+    val flutterSdkPath = System.getProperty("flutter.sdk") ?: error("flutter.sdk system property not set. Make sure you run via 'flutter build'.")
+
+    // هذا السطر هو المفتاح: يخبر Gradle بمكان إضافة Flutter
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
